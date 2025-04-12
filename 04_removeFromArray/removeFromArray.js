cea48de18108158319749c113b483f9c5b12a9f8
Fearless-Badger <micah.najacht@gmail.com>
2025-04-12 17:08:34 -0500

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums_to_delete) {
+    console.log(array);
+    console.log(nums_to_delete);
+
+    for ( let i = 0; i < array.length ; i++ ){
+        for ( num of nums_to_delete){
+            if (array[i] === num){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;

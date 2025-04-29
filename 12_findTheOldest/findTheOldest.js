e60db053b8023bc4eb2ee860c7032f9193665dc4
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 16:56:58 -0500

    Finish last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people = []) {
+  const cur_year = new Date().getFullYear();
+  let oldest = 0;
+  let result;
 
+  people.forEach(function (person) {
+    let cur_age;
+    if (person.yearOfDeath) {
+      cur_age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      cur_age = cur_year - person.yearOfBirth;
+    }
+
+    if (cur_age > oldest) {
+      oldest = cur_age;
+      result = person;
+    }
+  });
+
+  return result;
 };
 
 // Do not edit below this line

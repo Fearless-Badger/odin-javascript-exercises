c77a174cc2c548902d6c6652b7752bab9c763469
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 01:15:39 -0500

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a=0, b=0) {
+  return +(a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a=0, b=0) {
+	return +(a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr=[]) {
+	return arr.reduce( (sum, cur) => sum + cur , 0);
+}
+
+const multiply = function(arr=[]) {
+  return arr.reduce( (sum, cur ) => sum * cur, 1);
 };
 
-const multiply = function() {
-
+const power = function(a=0, b=0) {
+  return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a=1) {
+	let res = 1;
+  for (let i = a; i > 0; i-- ){
+    res *= i
+  }
+  return res;
 };
 
 // Do not edit below this line

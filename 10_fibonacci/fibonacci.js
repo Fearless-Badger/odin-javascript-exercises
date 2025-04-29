750c62585de2a235e7eba31914961295e3097504
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 02:09:45 -0500

    Finish fib exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let idx = +(num);
+
+    if (typeof( idx ) !== "number" || idx < 0){
+        return "OOPS";
+    }
+
+    if (idx === 0 ){
+        return 0;
+    } 
+    else if (idx === 1){
+        return 1;
+    }
+    
+
+    let res = 1;
+    let last = 1;
+    let temp;
+    for ( let i = 2 ; i < idx ; i++ ){
+        temp = res;
+        res += last;
+        last = temp;
+    }
+
+    return res;
 
 };
 

ec997c687da3755e79190066c2864b96552dfd1b
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 01:50:04 -0500

    Polish solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -6,26 +6,7 @@ const palindromes = function (str='') {
     const clean_lower = clean.map( char => char.toLowerCase());
     const reverse = [...clean_lower].reverse();
 
-    for (let i = 0; i < clean_lower.length ; i++ ){
-        if ( clean_lower[i] !== reverse[i] ){
-            return false;
-        }
-    }
-
-    return true;
-
-    // let str_arr = str.split("");
-    // const clean = str_arr.filter( char => (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9'));
-    // const clean_lower = clean.map((char) => char.toLowerCase());
-    // const reverse = [...clean_lower].reverse();
-    
-    // for (let i = 0; i < clean.length; i++ ){
-    //     if (clean_lower[i] !== reverse[i]){
-    //         return false;
-    //     }
-    // }
-
-    // return true;
+    return clean_lower.join("") === reverse.join("");
 
 };
 

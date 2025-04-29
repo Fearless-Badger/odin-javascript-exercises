e62831f520d9069ef042ec5d0c17894f92cc0ea2
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 01:46:34 -0500

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str='') {
+
+
+    let str_arr = str.split("");
+    const clean = str_arr.filter( char => (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || ( char >= "0" && char <= "9") );
+    const clean_lower = clean.map( char => char.toLowerCase());
+    const reverse = [...clean_lower].reverse();
+
+    for (let i = 0; i < clean_lower.length ; i++ ){
+        if ( clean_lower[i] !== reverse[i] ){
+            return false;
+        }
+    }
+
+    return true;
+
+    // let str_arr = str.split("");
+    // const clean = str_arr.filter( char => (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9'));
+    // const clean_lower = clean.map((char) => char.toLowerCase());
+    // const reverse = [...clean_lower].reverse();
+    
+    // for (let i = 0; i < clean.length; i++ ){
+    //     if (clean_lower[i] !== reverse[i]){
+    //         return false;
+    //     }
+    // }
+
+    // return true;
 
 };
 
